2781429fb7202b7f5260660e14f761ca1e7f591e
Pedro Maracajá <pedroomrj@gmail.com>
2021-06-13 15:09:10 -0300

    tela home

---
+++ src/page/Home/tileData.js
@@ -0,0 +1,69 @@
+const tileData = [
+    {
+      img: '../../assets/image/super1.jfif',
+      title: 'Breakfast',
+      author: 'jill111',
+      cols: 2,
+      featured: true,
+    },
+    {
+      img: '../../assets/image/super2.jfif',
+      title: 'Tasty burger',
+      author: 'director90',
+    },
+    {
+      img: '../../assets/image/super1.jfif',
+      title: 'Camera',
+      author: 'Danson67',
+    },
+    {
+      img: '../../assets/image/super1.jfif',
+      title: 'Morning',
+      author: 'fancycrave1',
+      featured: true,
+    },
+    {
+      img: '../../assets/image/super2.jfif',
+      title: 'Hats',
+      author: 'Hans',
+    },
+    {
+      img: '../../assets/image/super1.jfif',
+      title: 'Honey',
+      author: 'fancycravel',
+    },
+    {
+      img: '../../assets/image/super2.jfif',
+      title: 'Vegetables',
+      author: 'jill111',
+      cols: 2,
+    },
+    {
+      img: '../../assets/image/super3.jpg',
+      title: 'Water plant',
+      author: 'BkrmadtyaKarki',
+    },
+    {
+      img: '../../assets/image/super1.jfif',
+      title: 'Mushrooms',
+      author: 'PublicDomainPictures',
+    },
+    {
+      img: '../../assets/image/super2.jfif',
+      title: 'Olive oil',
+      author: 'congerdesign',
+    },
+    {
+      img: '../../assets/image/super1.jfif',
+      title: 'Sea star',
+      cols: 2,
+      author: '821292',
+    },
+    {
+      img: '../../assets/image/super3.jpg',
+      title: 'Bike',
+      author: 'danfador',
+    },
+  ];
+  
+  export default tileData;
--- src/routes.js
+++ src/routes.js
@@ -3,12 +3,14 @@ import { Route , BrowserRouter } from "react-router-dom";
 
 import Login from "./page/Login";
 import Register from "./page/Register";
+import Home from "./page/Home";
 
 const routes = () => {
     return (
         <BrowserRouter>
             <Route component={Login} path="/" exact />
             <Route component={Register} path="/register" />
+            <Route component={Home} path="/home" />
         </BrowserRouter>
     );
 }
