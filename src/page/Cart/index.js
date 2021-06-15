899586e88794dee03b03c7a3387d746311b13981
James <jamesmarcos2123@hotmail.com>
2021-06-15 07:59:39 -0300

    init list items

--- src/page/Cart/index.js
+++ src/page/Cart/index.js
@@ -1,29 +1,79 @@
-import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
+import React, { useState } from "react";
+import { makeStyles, withStyles } from '@material-ui/core/styles';
+import StepConnector from '@material-ui/core/StepConnector';
 import Stepper from '@material-ui/core/Stepper';
 import Step from '@material-ui/core/Step';
 import StepLabel from '@material-ui/core/StepLabel';
-import Button from '@material-ui/core/Button';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import Divider from '@material-ui/core/Divider';
+import ListItemText from '@material-ui/core/ListItemText';
+import logo from "../../assets/logo.jpg";
+import shopcart from "../../assets/shopcart.svg";
+import person from "../../assets/person.svg";
+import ListItemAvatar from '@material-ui/core/ListItemAvatar';
+import Avatar from '@material-ui/core/Avatar';
+import { Menu, Button, MenuItem, TextField, GridList, GridListTile, GridListTileBar, IconButton, StarBorderIcon } from "@material-ui/core";
+import Typography from '@material-ui/core/Typography';
+import { Link } from "react-router-dom";
+
+
+
+
+
+const ColorlibConnector = withStyles({
+    root: {
+        background: 'red'
+    },
+    line: {
+        height: 3,
+        border: 0,
+        backgroundColor: '#eaeaf0',
+        borderRadius: 1,
+    },
+})(StepConnector);
 
 const useStyles = makeStyles((theme) => ({
     root: {
         width: '100%',
+    },
+    list: {
+        width: '100%',
+        maxWidth: '100%',
+        backgroundColor: theme.palette.background.paper,
+    },
+    inline: {
+        display: 'flex',
+        flexDirection: 'row',
     },
     instructions: {
         marginTop: theme.spacing(1),
         marginBottom: theme.spacing(1),
     },
     fistStep: {
-        color: 'blue'
-    }
+        background: '#E5E4E2'
+    },
+    secondStep: {
+        background: '#E5E4E2'
+    },
+    gridList: {
+        display: "flex",
+        alignItems: "center",
+        transform: 'translateZ(0)'
+    },
 }));
 
+
+
+
 function getSteps() {
-    return ['Carrinho', 'Confimar Dados', 'Finalizar Compra'];
+    return ['Carrinho', 'Finalizar Compra'];
 }
+
 
 export default function Cart() {
     const classes = useStyles();
+    const [anchorEl, setAnchorEl] = useState(null);
     const [activeStep, setActiveStep] = React.useState(0);
     const steps = getSteps();
 
@@ -35,63 +85,82 @@ export default function Cart() {
         setActiveStep((prevActiveStep) => prevActiveStep - 1);
     };
 
+    const handleClose = () => {
+        setAnchorEl(null);
+    };
+
+    const handleClick = (event) => {
+        setAnchorEl(event.currentTarget);
+    };
 
     return (
+
         <div className={classes.root}>
-            <Stepper activeStep={activeStep} alternativeLabel>
-                {steps.map((label) => (
-                    <Step key={label}>
-                        <StepLabel>{label}</StepLabel>
-                    </Step>
-                ))}
-            </Stepper>
-            <div >
-                {activeStep === 0 ? (
-                    <div className={classes.fistStep}>
-                        <p> asdasdsa </p>
+            <div className="header">
+                <img height="100px" src={logo} />
+                <TextField id="standard-basic" label="Search" />
+                <div className="icons">
+                    <div className="carrinho">
+                        <Link to="/cart"><img src={shopcart} /></Link>
                     </div>
-                ) : (
-                    <div>
-                        {activeStep === 1 ? (
-                            <div>
-                                <h1>second step</h1>
-                            </div>
-                        ) : (
-                            <div>
-                                <h1>thrid step</h1>
-                            </div>
-                        )}
-                    </div>
-                    
-                )}
+                    <img onClick={handleClick} id="person" src={person} />
+                    <Menu
+                        anchorEl={anchorEl}
+                        keepMounted
+                        open={Boolean(anchorEl)}
+                        onClose={handleClose}
+                    >
+                        <MenuItem >Meus Pedidos</MenuItem>
+                        <MenuItem >Meus Endereços</MenuItem>
+                        <MenuItem >Sair</MenuItem>
+                    </Menu>
+                </div>
             </div>
-            <Button variant="contained" color="primary" onClick={handleNext}>
-                {activeStep === steps.length - 1 ? 'Finish' : 'Next'}
-            </Button>
-            {/* <div>
-        {activeStep === steps.length ? (
-          <div>
-            <Typography className={classes.instructions}>All steps completed</Typography>
-            <Button onClick={handleReset}>Reset</Button>
-          </div>
-        ) : (
-          <div>
-            <Typography className={classes.instructions}>{getStepContent(activeStep)}</Typography>
-            <div>
-              <Button
-                disabled={activeStep === 0}
-                onClick={handleBack}
-                className={classes.backButton}
-              >
-                Back
-              </Button>
-              <Button variant="contained" color="primary" onClick={handleNext}>
-                {activeStep === steps.length - 1 ? 'Finish' : 'Next'}
-              </Button>
+
+            <div className="body-cart">
+                <Stepper activeStep={activeStep} alternativeLabel connector={<ColorlibConnector />}>
+                    {steps.map((label) => (
+                        <Step key={label}>
+                            <StepLabel >{label}</StepLabel>
+                        </Step>
+                    ))}
+                </Stepper>
+                <div >
+                    {activeStep === 0 ? (
+                        <div className={classes.fistStep}>
+                            <List className={classes.list}>
+                                <ListItem alignItems="flex-start">
+                                    <ListItemAvatar>
+                                        <Avatar alt="imagem do produto" src="/static/images/avatar/1.jpg" />
+                                    </ListItemAvatar>
+                                    <ListItemText
+                                        primary="NOME DO PRODUTO"
+                                        secondary={
+                                            <div
+                                                className={classes.inline}
+                                            >
+                                                <p>Infos produto</p>
+                                                <p>Preço produto    </p>
+                                            </div>
+
+                                        }
+                                    />
+                                </ListItem>
+                                <Divider variant="inset" component="li" />
+                            </List>
+                        </div>
+                    ) : (
+                        <div className={classes.secondStep}>
+                            <h1>second step</h1>
+                        </div>
+
+                    )}
+                </div>
+                <Button variant="contained" color="primary" onClick={handleNext}>
+                    {activeStep === steps.length - 1 ? 'Finish' : 'Next'}
+                </Button>
+
             </div>
-          </div>
-        )}
-      </div> */}
         </div>
     );
 }
