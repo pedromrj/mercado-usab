bf0cef8fb046803eaee34661c4d8ff64871d2805
James <jamesmarcos2123@hotmail.com>
2021-06-16 17:16:00 -0300

    wip

--- src/page/ListItems/tileData.js
+++ src/page/ListItems/tileData.js
@@ -1,70 +1,84 @@
 const tileData = [
     {
+      key:1,
       name: "pipoca",
       img: "...",
       valor: "0,00"
     },
     {
+      key:2,
       name: "pipoca",
       img: "...",
       valor: "0,00"
     },
     {
+      key:3,
       name: "pipoca",
       img: "...",
       valor: "0,00"
     },
     {
+      key:4,
       name: "pipoca",
       img: "...",
       valor: "0,00"
     },
     {
+      key:5,
       name: "pipoca",
       img: "...",
       valor: "0,00"
     },
     {
+      key:6,
       name: "pipoca",
       img: "...",
       valor: "0,00"
     },
     {
+      key:7,
       name: "pipoca",
       img: "...",
       valor: "0,00"
     },
     {
+      key:8,
       name: "pipoca",
       img: "...",
       valor: "0,00"
     },
     {
+      key:9,
       name: "pipoca",
       img: "...",
       valor: "0,00"
     },
     {
+      key:10,
       name: "pipoca",
       img: "...",
       valor: "0,00"
     },
     {
+      key:11,
       name: "pipoca",
       img: "...",
       valor: "0,00"
     },
     {
+      key:12,
       name: "pipoca",
       img: "...",
       valor: "0,00"
     },
     {
+      key:13,
       name: "pipoca",
       img: "...",
       valor: "0,00"
     },
     {
+      key:14,
       name: "pipoca",
       img: "...",
       valor: "0,00"
