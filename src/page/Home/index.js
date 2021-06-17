01003ef4492ca02a67e5a02fc2484400f611d6a2
Pedro Maracajá <pedroomrj@gmail.com>
2021-06-16 22:09:29 -0300

    ajuste

--- src/page/Home/index.js
+++ src/page/Home/index.js
@@ -1,30 +1,45 @@
 import React, {useState} from "react";
 import {Link} from "react-router-dom";
 import "./styles.css";
-import {Menu, Button,MenuItem,TextField, GridList,GridListTile,GridListTileBar,IconButton,StarBorderIcon, makeStyles} from "@material-ui/core";
+import {RadioGroup, StyledRadio,Grid,Paper,ButtonBase,Typography,FormControlLabel,Radio,Menu, Button,MenuItem,TextField, GridList,GridListTile,GridListTileBar,IconButton,StarBorderIcon, makeStyles} from "@material-ui/core";
 import logo from "../../assets/logo.jpg";
 import shopcart from "../../assets/shopcart.svg";
 import person from "../../assets/person.svg";
 import data from './tileData';
-import image1 from "../../assets/image/super3.jpeg";
 import arrowDown from "../../assets/arrowup.svg";
+import dataProduto from "./produtos";
+import arrowUp from "../../assets/arrowdown.svg";
 
 const useStyles = makeStyles((theme) => ({
-    gridList: {
-      transform: 'translateZ(0)',
-      width: "97%"
+    root: {
+      flexGrow: 1,
     },
-    titleBar: {
-      background:
-        'linear-gradient(to bottom, rgba(255,153,0,0.7) 0%, ' +
-        'rgba(255,153,0,0.3) 70%, rgba(255,153,0,0) 100%)',
-    }
+    paper: {
+      padding: theme.spacing(1),
+      marginRight: "10px",
+      marginBottom: "10px",
+      maxWidth: 300,
+      maxHeight: 100
+    },
+    image: {
+      width: 100,
+      height: 128,
+    },
+    img: {
+      paddingBottom: "20px",
+      display: 'block',
+      maxWidth: '100%',
+      maxHeight: '100%',
+    },
   }));
 
 function Home() {
+    const classes = useStyles();
     const [tileData, setData] = useState(data);
     const [find, setFind] = useState(true);
-
+    const [findProduto, setFindProduto] = useState(true);
+    const [filtro, setFiltro] = useState({ name: "Supermercado", status: true});
+    const [produtos, setProdutos] = useState(dataProduto);
     const [anchorEl, setAnchorEl] = useState(null);
 
     const handleClick = (event) => {
@@ -35,7 +50,26 @@ function Home() {
       setAnchorEl(null);
     };
 
-    //TODO Ajustar filtro pois tornalo reativo para deixar só o que está na lista do filtro.
+    function handlerFiltro(e) {
+        setFiltro({ name: "true" === e.target.value ? "Supermercado" : "Produtos" , status : e.target.value === "true" });
+    }
+
+    function handlerChangeCategoria(e) {
+        var temp2 = dataProduto.filter(x => {
+            return x.name.includes(e.target.value);
+        });
+
+        if (e.target.value === "") {
+            setProdutos(dataProduto);
+            setFindProduto(false);
+            setFindProduto(true);       
+        } else {
+            setProdutos(temp2);
+            setFindProduto(false);
+            setFindProduto(true);
+        }
+    }
+
     function handlerChange(e) {
         var temp = data.filter(x => {
             return x.title.includes(e.target.value)
@@ -65,7 +99,7 @@ function Home() {
                     <strong>DESCRIÇÃO:</strong>
                     <p>{"Um local com preço ideal para você."}</p>
                     
-                    <Link to="/supermercado/items" >Selecionar</Link>
+                    <Link onClick={localStorage.setItem("nome-super", data.title)}to="/supermercado/items" >Selecionar</Link>
                 </li>
                 ))}
             </ul>
@@ -76,12 +110,64 @@ function Home() {
         return <ul><li>Não Encontrado</li></ul>;
     }
 
-    const classes = useStyles();
+    function getProdutos() {
+        return (<div>
+        {produtos.map((title) => (
+            <div key={title.key} className={classes.root}>
+              <Paper className={classes.paper}>
+                <Grid container spacing={1}>
+                  <Grid item>
+                    <ButtonBase className={classes.image}>
+                      <img className={classes.img} alt="complex" height="90" src={logo} />
+                    </ButtonBase>
+                  </Grid>
+                  <Grid item xs={12} sm container>
+                    <Grid item xs container direction="column" spacing={1}>
+                      <Grid item xs>
+                        <Typography gutterBottom variant="subtitle1">
+                          {title.name}
+                        </Typography>
+                      </Grid>
+                      <div className="qtditems">
+                        <img src={arrowUp}/>
+                        <label>1</label>
+                        <img src={arrowDown}/>
+                      </div>
+                      
+                    </Grid>
+                    <div className="valor">
+                        <label>R$: {title.valor}</label>
+                      </div>
+                  </Grid>
+                </Grid>
+              </Paper>
+            </div>
+        ))}</div>);
+    }
+
+    function handler() {
+        if (filtro.status) {
+            setFind(true);
+            return handlerChange;
+        } else {
+            setFindProduto(true);
+            return handlerChangeCategoria;
+        }        
+    }
+
     return (
         <div className="container-home">
             <div className="header">
                 <img height="100px" src={logo} />    
-                <TextField onChange={handlerChange} id="standard-basic" label="Search"/>
+                <div className="search-header">
+                    <div className="search-input">
+                        <TextField onChange={filtro.status === true ? handlerChange : handlerChangeCategoria} id="standard-basic" label="Search"/>
+                    </div>
+                    <RadioGroup onChange={handlerFiltro} defaultValue="true" aria-label="gender" name="filtro">
+                        <FormControlLabel value="true" control={<Radio />} label="Supermercado" />
+                        <FormControlLabel value="false" control={<Radio />} label="Categoria" />
+                    </RadioGroup>
+                </div>
                 <div className="icons">
                     <div className="carrinho">
                         <Link to="/carrinho"><img src={shopcart} /></Link>
@@ -102,8 +188,12 @@ function Home() {
             </div>
             <div className="body">
                     <div className="profile-container">
-                        <h2>Supermercados.</h2>
-                        {!find ? getDivVazia():getListaPopulada()}    
+                        <h2>{filtro.name}</h2>
+                        {console.log(filtro.status)}
+                        { filtro.status === true ?
+                            !find ? getDivVazia() : getListaPopulada() : 
+                            !findProduto ? getDivVazia() : getProdutos() 
+                        }     
                     </div>
                 
             </div>
---
+++ src/page/Home/produtos.js
@@ -0,0 +1,76 @@
+const tileData = [
+    {
+      key: 1,
+      name: "pipoca",
+      img: "...",
+      valor: "0,00"
+    },
+    { key: 2,
+      name: "refrigerante",
+      img: "...",
+      valor: "0,00"
+    },
+    { key: 3,
+      name: "suco",
+      img: "...",
+      valor: "0,00"
+    },
+    { key: 4,
+      name: "pipoca",
+      img: "...",
+      valor: "0,00"
+    },
+    { key: 5,
+      name: "pipoca",
+      img: "...",
+      valor: "0,00"
+    },
+    { key: 6,
+      name: "pipoca",
+      img: "...",
+      valor: "0,00"
+    },
+    { key: 7,
+      name: "pipoca",
+      img: "...",
+      valor: "0,00"
+    },
+    { key: 8,
+      name: "pipoca",
+      img: "...",
+      valor: "0,00"
+    },
+    { key: 9,
+      name: "pipoca",
+      img: "...",
+      valor: "0,00"
+    },
+    { key: 10,
+      name: "pipoca",
+      img: "...",
+      valor: "0,00"
+    },
+    { key: 11,
+      name: "pipoca",
+      img: "...",
+      valor: "0,00"
+    },
+    { key: 12,
+      name: "pipoca",
+      img: "...",
+      valor: "0,00"
+    },
+    { key: 13,
+      name: "pipoca",
+      img: "...",
+      valor: "0,00"
+    },
+    { key: 14,
+      name: "pipoca",
+      img: "...",
+      valor: "0,00"
+    },
+    
+  ];
+  
+  export default tileData;
