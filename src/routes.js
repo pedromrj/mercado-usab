d63776769a87c40190f779733b431dffbdfffe1e
James <jamesmarcos2123@hotmail.com>
2021-06-16 21:56:07 -0300

    adjusted in routes

--- src/routes.js
+++ src/routes.js
@@ -1,20 +1,21 @@
 import React from "react";
 import { Route , BrowserRouter } from "react-router-dom";
-import Cart from "./page/Cart";
 
 import Login from "./page/Login";
 import Register from "./page/Register";
 import Home from "./page/Home";
 import ListItems from "./page/ListItems"
+import Cart from "./page/Cart";
 
 const routes = () => {
     return (
         <BrowserRouter>
             <Route component={Login} path="/" exact />
             <Route component={Register} path="/register" />
-            <Route component={Cart} path="/cart" />
             <Route component={Home} path="/home" />
             <Route component={ListItems} path="/supermercado/items" />
+            <Route component={Cart} path="/cart" />
+
         </BrowserRouter>
     );
 }
